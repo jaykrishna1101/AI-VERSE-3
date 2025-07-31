9957e2d4849b5a3532003c000f5bdf6cd19d2bdc
MUCHKUNDRAJE <muchkundraje.thote@vectorlab.in>
2025-07-31 14:17:51 +0530

    added the landing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 
 import * as THREE from 'three';
+import Event from './components/Event';
 
 function ScrollCamera({ cameraPositions }) {
   const { camera } = useThree();
@@ -58,20 +59,19 @@ export default function App() {
 
 
   const cameraPositions = [
-    { scroll: 0, position: [3, 1,  -100] },
-    { scroll: 50, position: [30, 1, -110] },
-    { scroll: 20, position: [3, 1, -120] },
-    { scroll: 30, position: [3, 1, -130] },
-    { scroll: 40, position: [3, 1, -140] },
-    { scroll: 50, position: [3, 2, -140] },
-    { scroll: 100, position: [3, 30, -140] },
+    { scroll:0, position: [30, 100, -110] },
+    { scroll: 50, position: [  30, 8 , -100] },
   
+   
+
+
+
   ];
 
   return (
     <>
       <div className=' fixed top-0 left-0 w-full h-screen'  >
-        <Canvas camera={{ position: [3, 1, -100], fov: 70 }}>
+        <Canvas camera={{ position: [3, 10, -100], fov: 70 }}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[2, 2, 5]} intensity={1} />
           <Model scale={1} />
@@ -83,8 +83,23 @@ export default function App() {
       </div>
 
       <div className="scroll-content relative">
-        <section style={{ height: '200vh', padding: '5rem', color: 'white' }}>
-         
+        <section style={{ height: '400vh', padding: '0rem', color: 'white' }} >
+          <div  className='h-full w-full py-40'>
+                <div className='h-10  w-full  mb-10 flex items-center justify-center'>
+                 <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg" className='w-full h-full ' alt="" />
+                </div>
+              <div className='bebas-neue-regular p-1 w-full text-[230px] flex items-center justify-center rounded-xl shadow-2xl  bg-[#EE232C] text-white  '>
+                  <h1 className=' px-4 leading-60  '>
+                    Compufest 2K25
+                 </h1>
+              </div> 
+
+          </div>
+              
+
+                <div className='h-screen w-full  bg-red-200 '>
+                    <Event/>
+              </div>
         </section>
       </div>
 
---
+++ src/components/Event.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Event() {
+  return (
+    <div className='h-full w-full '>
+      Event
+
+    </div>
+  )
+}
+
+export default Event
