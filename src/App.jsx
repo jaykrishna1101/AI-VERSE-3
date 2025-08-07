7f1e1446ac1a5373addc237c230555501b5fb940
KritishBokde91 <kritishbokde.official@gmail.com>
2025-08-07 16:32:36 +0530

    Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link,Navigate } from 'react-router-dom';
 import Home from "./pages/Home"
 import Team from "./pages/Team"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
+    <>
+       <Analytics/>
     <BrowserRouter>
       <div>
         
@@ -19,5 +22,6 @@ export default function App() {
         </Routes>
       </div>
     </BrowserRouter>
+    </>
   );
 }
