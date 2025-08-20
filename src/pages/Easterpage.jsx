a483fd75802da270a3904193958b94b75e618c3b
MUCHKUNDRAJE <muchkundthote@gmail.com>
2025-08-20 21:11:35 +0530

    added-changes-in-the-easter

--- src/pages/Easterpage.jsx
+++ src/pages/Easterpage.jsx
@@ -34,7 +34,7 @@ useEffect(() => {
    <div ref={textRef} className='w-full opacity-0 md:w-3xl h-[80%] md:h-[27rem] rounded-2xl bg-white flex items-center flex-col justify-center gap-3 text-zinc-900'> 
           <h1 className='text-3xl'>You have unlock the Compufest Code  </h1>
           <h1 className='bg-neutral-800 px-10 py-2 text-center tracking-wider text-white text-4xl '>" with great power comes great responsibility "</h1>
-          <h1 className='spider text-3xl text-center px-20'>Find one of the three guys from the Following in the CT Department and say the Phrase to get the Cool Reward </h1>
+          <h1 className='spider text-3xl text-center px-20'> <span className='bg-red-500 px-3 text-white'>Mission :</span> Find one of the three guys from the following list in CT Department and say the Phrase above to get a cool reward </h1>
           <h1 className='text-2xl mt-2'>Muchkund Thote</h1>
           <h2 className='text-2xl'>Rachit Singh</h2>
           <h2 className='text-2xl'>Krtish Bokde </h2>
